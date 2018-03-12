3c1de74d2cff4c7a63b690730562c361cc33b4b4
cloudxpc <cloudxpc@126.com>
2018-03-12 22:39:01 +0800

    Initial commit

---
+++ src/main/java/com/uutic/uploadcenter/UploadcenterApplication.java
@@ -0,0 +1,12 @@
+package com.uutic.uploadcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadcenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadcenterApplication.class, args);
+	}
+}
